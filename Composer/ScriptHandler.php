46140bf0e411e44508d7889a414d24722f14592a
Akeem <akeem.philbert@wepala.com>
2015-01-16 12:47:11 -0400

    removed doctrine bundle

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -27,7 +27,6 @@ class ScriptHandler extends SensioScriptHandler
         $kernelFile = $appDir.'/AppKernel.php';
         $ref = 'new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),';//TODO replace with custom profiler here
         $bundleDeclaration = "new Doctrine\\Bundle\\PHPCRBundle\\DoctrinePHPCRBundle(),\n            ";
-        $bundleDeclaration .= "new Doctrine\\Bundle\\DoctrineBundle\\DoctrineBundle(),\n            ";
         $bundleDeclaration .= "new WeCMS\\AdminBundle\\WeCMSAdminBundle(),\n            ";
         $bundleDeclaration .= "new WeCMS\\SiteBundle\\WeCMSSiteBundle(),\n            ";
         $bundleDeclaration .= "new FOS\\UserBundle\\FOSUserBundle(),\n            ";
